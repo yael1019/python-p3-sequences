dc4f7da020fff65c39d7d72a5b9d29c79b59ad58
Yael Harosh <yaelharosh01@gmail.com>
2023-04-02 10:17:20 -0400

    pass tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list = []
+    if length > 0:
+        list.append(0)
+        if length > 1:
+            list.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    list.append(
+                        list[i - 1] + list[i - 2]
+                    )
+    print(list)
